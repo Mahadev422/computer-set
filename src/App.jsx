35f53a4d6451604dbd5fe6c6813c89c3a7885f8c
Mahadev <project9162595119@gmail.com>
2025-05-19 14:42:46 +0530

    add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber';
 import { Suspense, useRef, useState } from 'react';
 import { OrbitControls, Environment, Html } from '@react-three/drei';
+import { TbZoomCode } from "react-icons/tb";
+import { MdScreenRotationAlt } from "react-icons/md";
 
 // Computer setup components with improved details
 import Desk from './components/Desk';
@@ -12,13 +14,14 @@ import Monitor from './components/Monitor';
 
 
 // Main scene component
-const Scene = () => {
+const Scene = ({ rotation, zoom }) => {
+
   const groupRef = useRef();
   const { camera } = useThree();
-  
+
   // Add subtle animation
   useFrame((state, delta) => {
-    if (groupRef.current) {
+    if (groupRef.current && rotation) {
       groupRef.current.rotation.y += delta*0.5;
     }
   });
@@ -34,7 +37,7 @@ const Scene = () => {
       />
       <Environment preset="city" />
       
-      <group position={[0,0,-1.3]} ref={groupRef}>
+      <group position={[0,0,0]} ref={groupRef}>
         <Desk />
         <Monitor />
         <Keyboard />
@@ -46,7 +49,7 @@ const Scene = () => {
       </group>
       
       <OrbitControls 
-        enableZoom={false}
+        enableZoom={ zoom ? true : false }
         enablePan={true}
         minPolarAngle={Math.PI / 6}
         maxPolarAngle={Math.PI / 2}
@@ -58,14 +61,30 @@ const Scene = () => {
 
 // Main component
 const ComputerSetup3D = () => {
+
+  const [rotation, setRotation] = useState(true)
+  const [zoom, setZoom] = useState(false)
+ 
+
   return (
-  <div className="absolute inset-0 w-full">
-    <Canvas
+  <div className="absolute inset-0">
+    
+    <Canvas 
       shadows
-      camera={{ position: [0, 1, 5]}}
-      className="absolute inset-0 w-[70%]"
+      camera={{ position: [0, 1, -5]}}
+      className="absolute inset-0"
     >
-      <Scene />
+      <Html>
+        <div className='absolute top-[-350px] right-[-250px]'>
+          <button onClick={() => setZoom(!zoom)} className={`text-3xl ${ !zoom ? 'bg-red-400' : 'bg-green-400'} cursor-pointer p-2 rounded-2xl m-1`}>
+            <TbZoomCode />
+          </button>
+          <button onClick={() => setRotation(!rotation)}
+          className={`text-3xl ${rotation ? 'bg-red-400' : 'bg-green-400'} cursor-pointer p-2 rounded-2xl m-1`}>           <MdScreenRotationAlt /> 
+          </button>
+        </div>
+      </Html>
+      <Scene rotation={rotation} zoom={zoom} />
     </Canvas>
   </div>
   );
